85c4f56581c4e06ac74d01c0f2ad7852b62e75d4
Sam Skinner <93877248+SamSkinner01@users.noreply.github.com>
2022-12-01 19:19:48 -0500

    Update CudaMatrixMult.cu

--- CudaMatrixMult.cu
+++ CudaMatrixMult.cu
@@ -1,3 +1,4 @@
+
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
 #include <stdio.h>
@@ -5,7 +6,6 @@
 
 using namespace std;
 
-#define threads 32 // In 2D array 32 * 32 = 1024 threads
 
 int ct = 0;
 
@@ -13,6 +13,7 @@ void fill(int r, int c, int* o) {
     /*
         Fills matrix with r rows and c columns.
         Generates matrix in a 1D form.
+
         ct variable is used to change the seed for randomizing.
         Calling this function twice without it will cause both arrays
         to be filled with the same values.
@@ -31,7 +32,7 @@ void print(int r, int c, int* o) {
         Will print out the matrix given r rows and c columns.
         Assumes the matrix is stored as a 1D array.
     */
-
+    
     for (int i = 0; i < r; i++) {
         for (int j = 0; j < c; j++) {
             cout << o[i * c + j] << " ";
@@ -41,117 +42,116 @@ void print(int r, int c, int* o) {
     cout << endl << endl;
 }
 
-__global__ void mult_2_matrix(int* a, int* b, int* c, int a_r, int a_c, int b_r, int b_c, int size) {
-    int row = threadIdx.y + blockIdx.y * blockDim.y;
-    int col = threadIdx.x + blockIdx.x * blockDim.x;
-
-
-    c[row * b_c + col] = 0;
-
-    while (row < a_r && col < b_c) {
-
-        for (int k = 0; k < a_c; k++) {
-            c[row * b_c + col] += a[row * a_c + k] * b[k * b_c + col];
-        }
-
-        row += gridDim.y * blockDim.y;
-        //col += gridDim.x * blockDim.x;
-        
-    }
-
-
-}
-
-void verify(int* mat1, int mat1_m, int mat1_n, int mat2_n, int mat2_p, int* mat2, int* output) {
+void verify(int* a,int a_r, int a_c, int b_r, int b_c, int* b, int* output) {
     /*
         Multiplies two matricies together.
         This function assumes that the 2 matricies are
         stored as a 1D array.
     */
-    for (int i = 0; i < mat1_m; i++) {
-        for (int j = 0; j < mat2_p; j++) {
+    for (int i = 0; i < a_r; i++) {
+        for (int j = 0; j < b_c; j++) {
             int sum = 0;
-            for (int k = 0; k < mat2_n; k++) {
-                sum += mat1[i * mat1_n + k] * mat2[k * mat2_p + j];
+            for (int k = 0; k < b_r; k++) {
+                sum += a[i * a_c + k] * b[k * b_c + j];
             }
-            output[i * mat2_p + j] = sum;
+            output[i * b_c + j] = sum;
         }
+    }
+    
+}
 
-        
+__global__ void matrixMult(int*a, int a_r, int a_c, int b_r, int b_c, int* b, int* output) {
+    int row = threadIdx.y + blockIdx.y * blockDim.y;
+    int col = threadIdx.x + blockIdx.x * blockDim.y;
+    
+    if (row < a_r && col < b_c) {
+        int sum = 0;
+        for (int k = 0; k < b_r; k++)
+            sum += a[row * a_c + k] * b[k * b_c + col];
+
+        output[row * b_c + col] = sum;
     }
 
 }
 
-int main() {
-    int* a, a_r, a_c, a_n, * dev_a;
-    int* b, b_r, b_c, b_n, * dev_b;
-    int* c, c_n, * dev_c;
+int main(){
+    // Create matrices a and b, and resulting matrix c
+    int* a, a_r, a_c, a_n, *dev_a;
+    int* b, b_r, b_c, b_n, *dev_b;
+    int* c, c_n, *dev_c;
 
+    // User input
     cout << "Please enter rows and cols for matrix 1: ";
-    cin >> a_r >> a_c;
-    a_n = a_r * a_c;
-
+    cin >> a_r >> a_c;  // Enter values for row and columns
+    a_n = a_r * a_c;    // Calculate size
     cout << "Next matrix: ";
     cin >> b_r >> b_c;
+    while (b_r != a_c) {
+        cout << "Columns of Mat 1 and Rows of Mat 2 must match. Try Again: \n";
+        cin >> b_r >> b_c;
+    }
     b_n = b_r * b_c;
     c_n = a_r * b_c;
     cout << endl;
-    int* dev_col;
 
+    // Initialize all matrices
     a = new int[a_n];
     b = new int[b_n];
     c = new int[c_n];
 
+    // Fill a and b with random numbers
     fill(a_r, a_c, a);
     fill(b_r, b_c, b);
-     //cout << endl;
-     //print(a_r, a_c, a);
-     //cout << endl;
-     //print(b_r, b_c, b);
-      /*for (int i = 0; i < a_r; i++) {
-          for (int j = 0; j < b_c; j++) {
-              c[i * b_c + j] = 0;
-          }
-      }*/
 
+    // Initialize matrices on device
     cudaMalloc(&dev_a, a_n * sizeof(int));
     cudaMalloc(&dev_b, b_n * sizeof(int));
     cudaMalloc(&dev_c, c_n * sizeof(int));
-    cudaMalloc(&dev_col, sizeof(int));
 
+    // Copy matrices from host to device
     cudaMemcpy(dev_a, a, a_n * sizeof(int), cudaMemcpyHostToDevice);
     cudaMemcpy(dev_b, b, b_n * sizeof(int), cudaMemcpyHostToDevice);
     cudaMemcpy(dev_c, c, c_n * sizeof(int), cudaMemcpyHostToDevice);
-    cudaMemcpy(dev_col, &a_c, sizeof(int), cudaMemcpyHostToDevice);
+   
+    // Create Timer
+    cudaEvent_t     start, stop;
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
+    cudaEventRecord(start, 0);
 
+    // Values for threads and blocks
+    dim3 thread(32, 32, 1);
+    dim3 block(100, 100, 1);
 
-    mult_2_matrix << < (c_n * 1023)/1024, threads >> > (dev_a, dev_b, dev_c, a_r, a_c, b_r, b_c, c_n);
-
-
-
+    // Mult Matr
+    matrixMult << < block, thread >> > (dev_a, a_r, a_c, b_r, b_c, dev_b, dev_c);
+    
+    // Stop timer, calc time and print
+    cudaEventRecord(stop, 0);
+    cudaEventSynchronize(stop);
+    printf("\nFinished Calculating With Cuda.\n");
+    float   elapsedTime;
+    cudaEventElapsedTime(&elapsedTime, start, stop);
+    printf("Time to do calculate:  %3.1f ms\n", elapsedTime);
     cudaMemcpy(c, dev_c, c_n * sizeof(int), cudaMemcpyDeviceToHost);
 
-    printf("CUDA FINISHED\n");
+    // Verify
+    printf("\nStarting Verification...\n");
+    bool isSame = true;
     int* ver = new int[c_n];
     verify(a, a_r, a_c, b_r, b_c, b, ver);
 
-    /*printf("ANSWER: \n");
-     print(a_r, b_c, ver);
-
-     printf("GOT: \n");
-     print(a_r, b_c, c);*/
-
     for (int i = 0; i < c_n; i++) {
         if (c[i] != ver[i]) {
-            cout << "FAILED FAILED\n";
-            cout << c[i] << "\t" << ver[i] << " " << i;
+            isSame = false;
+            printf("i: %d, c[i]: %d, ver[i]: %d\n", i, c[i], ver[i]);
             break;
         }
     }
 
-    cout << endl;
-    //print(a_r, b_c, c);
-
-
-
+    // Print verification boolean
+    if (isSame)
+        cout << "Success.\n";
+    else
+        cout << "Failed...\n";
 }
